47706b84fbec106f6bdda8757e0a8c461f04904f
Adithya Sunil <adithyasunil26@gmail.com>
2021-10-11 05:46:35 +0530

    Sweeping

--- fusesoc-sweep.py
+++ fusesoc-sweep.py
@@ -7,12 +7,13 @@ import itertools
 import numpy as np
 
 user = os.getlogin()
-path = "/Users/"+user+"/.cache/fusesoc/"
+path = "/home/"+user+"/.cache/fusesoc/"
 
 def main():
 
   core_name = sys.argv[1]
-  
+  core_name_cpy = core_name+"_cpy"
+  os.system("cp fusesoc_libraries/basejump/bsg_misc/{}.core fusesoc_libraries/basejump/bsg_misc/{}.core".format(core_name, core_name_cpy))
   core_file = open("fusesoc_libraries/basejump/bsg_misc/{}.core".format(core_name))
   parsed_yaml_file = yaml.load(core_file, Loader=yaml.FullLoader)
   path_in_core = parsed_yaml_file["filesets"]["makefile"]["files"][0]
@@ -42,7 +43,7 @@ def main():
 
   param_name = []
   param_values = []
-  while len(params) is not 0:
+  while len(params)!=0:
     k = params.pop()
     param_name.append(k[0].lower())
     param_values.append(k[1].split(" "))
@@ -50,7 +51,7 @@ def main():
   comb = list(itertools.product(*param_values))
 
   for i in range(len(comb)):
-    core_file_write = open("fusesoc_libraries/basejump/bsg_misc/{}.core".format(core_name),'w')
+    core_file_write = open("fusesoc_libraries/basejump/bsg_misc/{}.core".format(core_name_cpy),'w')
     j = parsed_yaml_file["targets"]["verilator_tb"]["parameters"]
     k = j[0].split("=")
     k[1] = comb[i][0]
@@ -62,8 +63,9 @@ def main():
     k[1] = comb[i][2]
     j[2] = "=".join(k)
     parsed_yaml_file["targets"]["verilator_tb"]["parameters"] = j
+    parsed_yaml_file["name"]=core_name_cpy
     yaml.dump(parsed_yaml_file,core_file_write)
-    os. system('fusesoc run --target=verilator_tb {}'.format(core_name))
+    os. system('fusesoc run --target=verilator_tb {}'.format(core_name_cpy))
 
 if __name__ == "__main__":
   main()
